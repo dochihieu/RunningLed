4f6bc095fba1c5a380568861b6b120081b470973
chihieu <hieu1804@techmasters-imac-3.local>
2016-04-04 11:42:57 +0700

    Initial Commit

---
+++ RunningLed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunningLed
+//
+//  Created by chihieu on 4/4/16.
+//  Copyright © 2016 chihieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunningLed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunningLed
+//
+//  Created by chihieu on 4/4/16.
+//  Copyright © 2016 chihieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
